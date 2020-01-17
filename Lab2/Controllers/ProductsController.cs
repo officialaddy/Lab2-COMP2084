16136c8543863b038078f39147224caaea762219
Aditya Jangra <200434215@srudent.georgianc.on.ca>
2020-01-16 21:07:48 -0500

    Add project files.

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string details(string id)
+        {
+            string message = "This is the product = " + id; 
+            return message;
+
+        }
+
+    }
+}
